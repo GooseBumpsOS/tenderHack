0f45200ccfecd3090d4e4859b859cba0d1bf2aeb
Georgy <goosebumpsmail2@gmail.com>
2020-01-26 01:31:33 +0300

    сделал апи по выборке ктру

--- src/Controller/MainApiController.php
+++ src/Controller/MainApiController.php
@@ -5,29 +5,42 @@ namespace App\Controller;
 use App\Service\ParserApi;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainApiController extends AbstractController
 {
     /**
-     * @Route("/{slug}", name="main_api")
+     * @Route("/ktru", name="main_api")
      */
-    public function index($slug)
+    public function index(Request $request)
     {
 
-//        $this->getDoctrine()->getManager(
-
-
-        $html = file_get_contents('https://edu.upt24.ru/sku/view/1387181');
-
-        echo str_replace('<base href="/"/>', '<base href="https://old.zakupki.mos.ru"/>', $html);
+        $okpdArray = $this->_getRawHtml($request->query->get('okpd2'));
 
         return $this->json([
+
+            $okpdArray
 
         ]);
     }
 
-    public function hello(){
-        echo "Hello";
+    private function _getRawHtml($okpd2){
+
+
+        $crawler = new Crawler(file_get_contents('https://zakupki.gov.ru/epz/ktru/search/results.html?searchString='. $okpd2 .'.121&morphology=on&search-filter=%D0%94%D0%B0%D1%82%D0%B5+%D1%80%D0%B0%D0%B7%D0%BC%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D1%8F&activeESCKLP=on&clGroupHiddenId=0&ktruCharselectedTemplateItem=0&sortBy=ITEM_CODE&pageNumber=1&sortDirection=true&recordsPerPage=_10&showLotsInfoHidden=false&rubricatorIdSelected=-1'));
+        $okpd2Array = [];
+
+        for ($i = 0; $i < $crawler->filter('.registry-entry__header-mid__number')->count(); $i++)
+        {
+
+            array_push($okpd2Array, [$crawler->filter('.registry-entry__header-mid__number > a')->eq($i)->text(), $crawler->filter('.w-space-inherit')->eq($i)->text()]);
+
+
+        }
+
+        return $okpd2Array;
+
     }
 }
