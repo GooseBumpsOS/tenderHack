c944c9b4fec976f0ce492c89d893870f1b3674ad
Georgy <goosebumpsmail2@gmail.com>
2020-01-26 02:34:41 +0300

    добавил поиск по окпд

--- src/Controller/MainApiController.php
+++ src/Controller/MainApiController.php
@@ -12,35 +12,45 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainApiController extends AbstractController
 {
     /**
-     * @Route("/ktru", name="main_api")
+     * @Route("/api", name="main_api")
      */
     public function index(Request $request)
     {
 
-        $okpdArray = $this->_getRawHtml($request->query->get('okpd2'));
+        $okpd = $this->_getOkpd($request->query->get('CTE'));
+        $ktruArray = $this->_getKtru($okpd);
 
         return $this->json([
 
-            $okpdArray
+            $okpd, $ktruArray
 
         ]);
     }
 
-    private function _getRawHtml($okpd2){
+    private function _getOkpd($cte){
+
+       $json = json_decode(file_get_contents('https://old.zakupki.mos.ru/api/Cssp/Sku/GetEntity?id=' . $cte), true);
+
+       return $json['production']['okpds'][0]['okpd']['code'];
+
+    }
+
+    private function _getKtru($okpd2){
 
 
-        $crawler = new Crawler(file_get_contents('https://zakupki.gov.ru/epz/ktru/search/results.html?searchString='. $okpd2 .'.121&morphology=on&search-filter=%D0%94%D0%B0%D1%82%D0%B5+%D1%80%D0%B0%D0%B7%D0%BC%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D1%8F&activeESCKLP=on&clGroupHiddenId=0&ktruCharselectedTemplateItem=0&sortBy=ITEM_CODE&pageNumber=1&sortDirection=true&recordsPerPage=_10&showLotsInfoHidden=false&rubricatorIdSelected=-1'));
-        $okpd2Array = [];
+
+        $crawler = new Crawler(file_get_contents('https://zakupki.gov.ru/epz/ktru/search/results.html?searchString='. $okpd2 .'&morphology=on'));
+        $ktru2Array = [];
 
         for ($i = 0; $i < $crawler->filter('.registry-entry__header-mid__number')->count(); $i++)
         {
 
-            array_push($okpd2Array, [$crawler->filter('.registry-entry__header-mid__number > a')->eq($i)->text(), $crawler->filter('.w-space-inherit')->eq($i)->text()]);
+            array_push($ktru2Array, [$crawler->filter('.registry-entry__header-mid__number > a')->eq($i)->text(), $crawler->filter('.w-space-inherit')->eq($i)->text()]);
 
 
         }
 
-        return $okpd2Array;
+        return $ktru2Array;
 
     }
 }
